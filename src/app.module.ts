8e04a25e4e6e8c8d1fe2b77fb8c4e2449d1e1966
DilkiThakshmila <dilkithakshmila@gmail.com>
2024-02-03 18:55:01 +0530

    Add MulterModule configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { MulterModule } from '@nestjs/platform-express'; // Import MulterModule
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ImageSchema } from './image.schema'; // Import ImageSchema here
@@ -8,6 +9,7 @@ import { ImageSchema } from './image.schema'; // Import ImageSchema here
   imports: [
     MongooseModule.forRoot('mongodb+srv://dilki:ADbt20@cluster0.rlyibbt.mongodb.net/?retryWrites=true&w=majority'), // Hardcode MongoDB connection URL here
     MongooseModule.forFeature([{ name: 'Image', schema: ImageSchema }]), // Import and define ImageSchema here
+    MulterModule.register({ dest: './uploads' }), // Add MulterModule configuration
   ],
   controllers: [AppController],
   providers: [AppService],
