6a6e2461b13f7b58243a11a2c8ec213b36d17834
DilkiThakshmila <dilkithakshmila@gmail.com>
2024-02-03 13:54:44 +0530

    image upload app

---
+++ src/app.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Get, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('image'))
+  async uploadFile(@UploadedFile() file: Express.Multer.File) {
+    return this.appService.uploadFile(file);
+  }
+
+  @Get('images')
+  async getImages() {
+    return this.appService.getImages();
+  }
+}
