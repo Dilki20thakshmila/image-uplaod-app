88f6b81202ebfcd4e3e6cee66cbdbf771f5a5a55
DilkiThakshmila <dilkithakshmila@gmail.com>
2024-02-04 19:35:46 +0530

    install dependancies

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,15 +1,18 @@
+// app.module.ts
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { MulterModule } from '@nestjs/platform-express'; // Import MulterModule
+import { MulterModule } from '@nestjs/platform-express';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ImageSchema } from './image.schema'; // Import ImageSchema here
+import { ImageSchema } from './image.schema';
+import { CorsModule } from 'cors'; // Import CorsModule
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://dilki:ADbt20@cluster0.rlyibbt.mongodb.net/?retryWrites=true&w=majority'), // Hardcode MongoDB connection URL here
-    MongooseModule.forFeature([{ name: 'Image', schema: ImageSchema }]), // Import and define ImageSchema here
-    MulterModule.register({ dest: './uploads' }), // Add MulterModule configuration
+    MongooseModule.forRoot('mongodb+srv://dilki:ADbt20@cluster0.rlyibbt.mongodb.net/?retryWrites=true&w=majority'),
+    MongooseModule.forFeature([{ name: 'Image', schema: ImageSchema }]),
+    MulterModule.register({ dest: './uploads' }),
+    CorsModule.forRoot({ origin: 'http://localhost:3000' }), // Configure CORS
   ],
   controllers: [AppController],
   providers: [AppService],
